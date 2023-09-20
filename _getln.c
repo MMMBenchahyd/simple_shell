7770e4d29de0ffddfc8ff12167a9e971eb8ea861
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2023-09-20 11:19:38 +0100

    update

--- _getln.c
+++ _getln.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
-* _getln - read one line from the prompt.
-* @data_ptr: struct for the program's data
+* _getln - recite the first line of the prompt.
+* @data_ptr: poiter of the program's data
 *
 * Return: reading counting bytes.
 */
@@ -13,33 +13,27 @@ int _getln(prog_s_data *data_ptr)
 	static char arr_opr[10] = {'\0'};
 	ssize_t byte_rd, i = 0;
 
-	/* check if doesnot exist more commands in the array */
-	/* and checks the logical operators */
+
 	if (!arr_cmnd[0] || (arr_opr[0] == '&' && errno != 0) ||
 		(arr_opr[0] == '|' && errno == 0))
 	{
-		/*free the memory allocated in the array if it exists */
 		for (i = 0; arr_cmnd[i]; i++)
 		{
 			free(arr_cmnd[i]);
 			arr_cmnd[i] = NULL;
 		}
 
-		/* read from the file descriptor int to buffer */
 		byte_rd = read(data_ptr->fil_dscription, &buffer, BUFFER_SIZE - 1);
 		if (byte_rd == 0)
 			return (-1);
 
-		/* split lines for \n or ; */
 		i = 0;
 		do {
 			arr_cmnd[i] = string_dup(ben_string_tok(i ? NULL : buffer, "\n;"));
-			/*checks and split for && and || operators*/
 			i = chk_for_logic_oprts(arr_cmnd, i, arr_opr);
 		} while (arr_cmnd[i++]);
 	}
 
-	/*obtains the next command (command 0) and remove it for the array*/
 	data_ptr->in_ln = arr_cmnd[0];
 	for (i = 0; arr_cmnd[i]; i++)
 	{
@@ -52,10 +46,10 @@ int _getln(prog_s_data *data_ptr)
 
 
 /**
-* chk_for_logic_oprts - checks and split for && and || operators
-* @arr_cmnd: array of the commands.
-* @i: index in the arr_cmnd to be checked
-* @arr_opr: array of the logical operators for each previous command
+* chk_for_logic_oprts - checks and splitting for the operators && and ||.
+* @arr_cmnd: a list of commands.
+* @i: to be checked is the index in the arr_cmnd.
+* @arr_opr: array containing the logical operators for every command before that
 *
 * Return: index of the last command in the arr_cmnd.
 */
@@ -64,12 +58,10 @@ int chk_for_logic_oprts(char *arr_cmnd[], int i, char arr_opr[])
 	char *tmp = NULL;
 	int j;
 
-	/* checks for the & char in the command line*/
 	for (j = 0; arr_cmnd[i] != NULL  && arr_cmnd[i][j]; j++)
 	{
 		if (arr_cmnd[i][j] == '&' && arr_cmnd[i][j + 1] == '&')
 		{
-			/* split the line when chars && was found */
 			tmp = arr_cmnd[i];
 			arr_cmnd[i][j] = '\0';
 			arr_cmnd[i] = string_dup(arr_cmnd[i]);
@@ -81,7 +73,7 @@ int chk_for_logic_oprts(char *arr_cmnd[], int i, char arr_opr[])
 		}
 		if (arr_cmnd[i][j] == '|' && arr_cmnd[i][j + 1] == '|')
 		{
-			/* split the line when chars || was found */
+
 			tmp = arr_cmnd[i];
 			arr_cmnd[i][j] = '\0';
 			arr_cmnd[i] = string_dup(arr_cmnd[i]);
--- env_mngmnt1.c
+++ env_mngmnt1.c
@@ -1,40 +1,38 @@
 #include "shell.h"
 
 /**
- * env_ky_gt - gets the value of an environment variable
- * @ky: the environment variable of interest
- * @data_ptr: struct of the program's data
- * Return: a pointer to the value of the variable or NULL if it doesn't exist
+ * env_ky_gt - obtains a variable's value in the environment
+ * @ky: the relevant environment variable
+ * @data_ptr: the pointer of the program's data
+ * Return: a pointer to the variable's value or NULL if it isn't there.
  */
 char *env_ky_gt(char *ky, prog_s_data *data_ptr)
 {
 	int i, ky_len = 0;
 
-	/* validate the arguments */
+
 	if (ky == NULL || data_ptr->envirmnt == NULL)
 		return (NULL);
 
-	/* obtains the leng of the variable requested */
 	ky_len = string_len(ky);
 
 	for (i = 0; data_ptr->envirmnt[i]; i++)
-	{/* Iterates through the environ and check for coincidence of the vame */
+	{
 		if (string_cmpr(ky, data_ptr->envirmnt[i], ky_len) &&
 		 data_ptr->envirmnt[i][ky_len] == '=')
-		{/* returns the value of the ky NAME=  when find it*/
+		{
 			return (data_ptr->envirmnt[i] + ky_len + 1);
 		}
 	}
-	/* returns NULL if did not find it */
 	return (NULL);
 }
 
 /**
- * env_ky_st - overwrite the value of the environment variable
- * or create it if does not exist.
- * @ky: name of the variable to set
+ * env_ky_st - replace the environment variable's value with 
+ * a new one or, if necessary, create one.
+ * @ky: the variable's name to set
  * @val: new value
- * @data_ptr: struct of the program's data
+ * @data_ptr: the pointer of the program's data
  * Return: 1 if the parameters are NULL, 2 if there is an erroror 0 if sucess.
  */
 
@@ -42,31 +40,27 @@ int env_ky_st(char *ky, char *val, prog_s_data *data_ptr)
 {
 	int i, ky_len = 0, nu_ky = 1;
 
-	/* validate the arguments */
 	if (ky == NULL || val == NULL || data_ptr->envirmnt == NULL)
 		return (1);
 
-	/* obtains the leng of the variable requested */
 	ky_len = string_len(ky);
 
 	for (i = 0; data_ptr->envirmnt[i]; i++)
-	{/* Iterates through the environ and check for coincidence of the vame */
+	{
 		if (string_cmpr(ky, data_ptr->envirmnt[i], ky_len) &&
 		 data_ptr->envirmnt[i][ky_len] == '=')
-		{/* If key already exists */
+		{
 			nu_ky = 0;
-			/* free the entire variable, it is new created below */
 			free(data_ptr->envirmnt[i]);
 			break;
 		}
 	}
-	/* make an string of the form key=value */
+
 	data_ptr->envirmnt[i] = string_conc(string_dup(ky), "=");
 	data_ptr->envirmnt[i] = string_conc(data_ptr->envirmnt[i], val);
 
 	if (nu_ky)
-	{/* if the variable is new, it is create at end of actual list and we need*/
-	/* to put the NULL value in the next position */
+	{
 		data_ptr->envirmnt[i + 1] = NULL;
 	}
 	return (0);
