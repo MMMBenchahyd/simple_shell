3956d98bb8261b52f3eace95119ffade7d59ad71
zakariiia <zakariamoukrim57@gmail.com>
2023-09-19 17:56:55 -0700

    editing

--- alias_managmnt.c
+++ alias_managmnt.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * print_alias - add, remove or show aliases
- * @data_ptr: struct for the program's data
- * @alias: name of the alias to be printed
- * Return: zero if sucess, or other number if its declared in the arguments
+ * print_alias - this func show, add, or remove aliases
+ * @data_ptr: this struct is for the data of  program's
+ * @alias: the name of the alias that will be printed
+ * Return: return 0 if sucess, or other number if it was  declared in the one of the arguments
  */
 int print_alias(prog_s_data *data_ptr, char *alias)
 {
@@ -38,75 +38,70 @@ int print_alias(prog_s_data *data_ptr, char *alias)
 }
 
 /**
- * get_alias - add, remove or show aliases
- * @data_ptr: struct for the program's data
- * @name: name of the requested alias.
- * Return: zero if sucess, or other number if its declared in the arguments
+ * get_alias - this will  add,show or  remove aliases
+ * @data_ptr: this struct for the data of the program
+ * @name: the name of the main requested alias.
+ * Return: return 0 if sucess, or other number if it was declared in the one of the arguments
  */
 char *get_alias(prog_s_data *data_ptr, char *name)
 {
 	int i, alias_len;
 
-	/* validate the arguments */
 	if (name == NULL || data_ptr->lst_of_alias == NULL)
 		return (NULL);
 
 	alias_len = string_len(name);
 
 	for (i = 0; data_ptr->lst_of_alias[i]; i++)
-	{/* Iterates through the environ and check for coincidence of the varname */
+	{
 		if (string_cmpr(name, data_ptr->lst_of_alias[i], alias_len) &&
 			data_ptr->lst_of_alias[i][alias_len] == '=')
-		{/* returns the value of the key NAME=  when find it */
+		{
 			return (data_ptr->lst_of_alias[i] + alias_len + 1);
 		}
 	}
-	/* returns NULL if did not find it */
 	return (NULL);
 
 }
 
 /**
- * set_alias - add, or override alias
- * @alias_string: alias to be seted in the form (name='value')
- * @data_ptr: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * set_alias - this  will add or override alias
+ * @alias_string: the alias that will  be seted in the form ( name='value' )
+ * @data_ptr: this struct is  for the program's data
+ * Return: return 0 if sucess, or other number if it was  declared in the one of the arguments
  */
 int set_alias(char *alias_string, prog_s_data *data_ptr)
 {
 	int i, j;
 	char buf[250] = {'0'}, *temp = NULL;
 
-	/* validate the arguments */
 	if (alias_string == NULL ||  data_ptr->lst_of_alias == NULL)
-		return (1);
-	/* Iterates alias to find = char */
+		return (1); 
+	
 	for (i = 0; alias_string[i]; i++)
 		if (alias_string[i] != '=')
 			buf[i] = alias_string[i];
 		else
-		{/* search if the value of the alias is another alias */
+		{
 			temp = get_alias(data_ptr, alias_string + i + 1);
 			break;
 		}
 
-	/* Iterates through the alias list and check for coincidence of the varname */
 	for (j = 0; data_ptr->lst_of_alias[j]; j++)
 		if (string_cmpr(buf, data_ptr->lst_of_alias[j], i) &&
 			data_ptr->lst_of_alias[j][i] == '=')
-		{/* if the alias alredy exist */
+		{
 			free(data_ptr->lst_of_alias[j]);
 			break;
 		}
 
-	/* add the alias */
 	if (temp)
-	{/* if the alias already exist */
+	{
 		buf_ad(buf, "=");
 		buf_ad(buf, temp);
 		data_ptr->lst_of_alias[j] = string_dup(buf);
 	}
-	else /* if the alias does not exist */
+	else
 		data_ptr->lst_of_alias[j] = string_dup(alias_string);
 	return (0);
 }
